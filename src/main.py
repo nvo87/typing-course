5f2a43e40d69cc7ce885959fa9a48e7c6a23843c
Slava Nabatchikov <nvo87@yandex.ru>
2022-10-28 20:34:05 +0300

    init commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+"""
+1. read csv
+2. get users.json https://jsonplaceholder.typicode.com/users/
+3. read users.json
+4. find id by email
+5. get post, albums,todos
+6. add post, albums,todos to user.json
+7. save users/${userId}/user.json to disk
+8. logging!
+"""
